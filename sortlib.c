84e7e6390094ae5350cac2620c6605764d8d2ea6
Tonda <tondasamu@gmail.com>
2024-10-04 21:26:48 +0200

    sorry i think i got it now :)

--- sortlib.c
+++ sortlib.c
@@ -4,6 +4,6 @@ void merge_sort (void **base, size_t nitems, size_t size, int (*compar)(const vo
 
 }
 
-void merge_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*)) {
+void quick_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*)) {
 
 }
