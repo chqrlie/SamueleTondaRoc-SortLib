6e60a2143b72d7e83fc6a4daf52f1ca3bdc687cc
Tonda <tondasamu@gmail.com>
2024-10-04 21:32:10 +0200

    added comment

--- sortlib.c
+++ sortlib.c
@@ -1,9 +1,21 @@
 #include "sortlib.h"
 
-void merge_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*)) {
+/*
+    @param base
+        pointer to the first element of the arry
+    @param nitems
+        lenght of the array
+    @param size
+        size of array items
+    @param compar
+        compare function
+*/
+void merge_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*)) 
+{
 
 }
 
-void quick_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*)) {
+void quick_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*)) 
+{
 
 }
