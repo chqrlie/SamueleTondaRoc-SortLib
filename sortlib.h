eefcd450402a8dd494d93e16ad2bbb60005e7c69
SamueleTondaRoc <tondasamu@gmail.com>
2024-10-05 18:25:26 +0200

    minor polish

--- sortlib.h
+++ sortlib.h
@@ -2,6 +2,7 @@
 #define SORTLIB
 
 #include <stdio.h>
+#include <stdbool.h>
 
 void merge_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*));
 void quick_sort (void **base, size_t nitems, size_t size, int (*compar)(const void*, const void*));
